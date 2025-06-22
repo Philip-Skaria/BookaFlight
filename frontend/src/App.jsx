b041b8aff6c290b927934abd1e69e388145b32ef
Philip Skaria <philipskaria04@gmail.com>
2025-06-22 20:05:05 +0530

    routing fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
-import Signup from "./pages/Signup.jsx"
-import Login from "./pages/Login.jsx"
+import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
 
-function App() {
+export const App = () => {
   return (
-    <>
-    <Signup/>
-    <Login/>
-    </>
+  <>
+    <Router>
+      <div>
+      <Link to="/signup">Signup</Link>
+      <Link to="/login">Login</Link>
+      </div>
+      <Routes>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
+  </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,20 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from './pages/login.jsx'
-import Signup from './pages/Signup.jsx'
-import{createBrowserRouter,RouterProvider} from "react-router-dom";
-import NotFoundPage from './pages/NotFoundPage.jsx'
 
-const router=createBrowserRouter([
-  {path:"/",element:<App/>},
-  {path:"/signup",element:<Signup/>},
-  {path:"/login",element:<Login/>},
-  {path:"*",element:<NotFoundPage/>}
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </StrictMode>,
 )
