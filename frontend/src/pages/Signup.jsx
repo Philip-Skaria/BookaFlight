db4a83e5896310d3e040da4059b026c8154d8ecf
tims-exe <timonkjohn@gmail.com>
2025-06-22 18:18:06 +0530

    fixed tailwind

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Signup = () => {
   return (
-    <div className="text-red">Signup</div>
+    <div className="text-2xl font-bold text-red-500">Signup</div>
   )
 }
