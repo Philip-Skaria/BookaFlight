6f71571b71438a5da41815f1d5b87fba2519771e
tims-exe <timonkjohn@gmail.com>
2025-06-23 22:34:31 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('path');
 const authRoutes = require('./routes/auth');
 const morgan  = require('morgan');
+const cors = require('cors');
 
 const app = express()
 const PORT = 5000;
+
+app.use(cors());
 
 app.use(morgan('dev'));
 
